8385f297cfe99c8be46283e9fc30045bfb4eac6b
Romuald Brillout <git@brillout.com>
2023-09-24 21:54:47 +0200

    support BurdaForward globbing

--- route.spec.ts
+++ route.spec.ts
@@ -9,9 +9,9 @@ describe('route()', () => {
     expect(test('/a/b', '/a/b')).toEqual([])
   })
   it('globbing', () => {
+    expect(test('/a/*/c', '/a/b/c')).toEqual(['b'])
     expect(test('/a/*/c', '/a/b/d')).toEqual(false)
     expect(test('/a/*', '/a/b/c')).toEqual(['b/c'])
-    expect(test('/a/*/c', '/a/b/c')).toEqual(['b'])
     expect(test('/*', '/a')).toEqual(['a'])
     expect(test('/*', '/a/b/c/d')).toEqual(['a/b/c/d'])
     expect(test('/a/*', '/a/b/c/d')).toEqual(['b/c/d'])
@@ -20,6 +20,19 @@ describe('route()', () => {
     expect(test('/a/*/c/*/e', '/a/b/c/d/e')).toEqual(['b', 'd'])
     expect(test('/a/*/c/*', '/a/b/c/d/e')).toEqual(['b', 'd/e'])
     expect(test('/a/*/e', '/a/b/c/d/e')).toEqual(['b/c/d'])
+  })
+  it('globbing - inside segment', () => {
+    expect(test('/a*e', '/a/b/c/d/e')).toEqual(['/b/c/d/'])
+  })
+  it('BurdaForward', () => {
+    // Use case 1
+    expect(test('/some/*.html', '/some/a.html')).toEqual(['a'])
+    expect(test('/some/*.html', '/some/a/b/c.html')).toEqual(['a/b/c'])
+    expect(test('/some/*.html', '/some/a/b/c.html')).toEqual(['a/b/c'])
+    // Use case 2
+    expect(test('/some/route*.html', '/some/route.html')).toEqual([''])
+    expect(test('/some/route*.html', '/some/route-a.html')).toEqual(['-a'])
+    expect(test('/some/route*.html', '/some/routea/b/c.html')).toEqual(['a/b/c'])
   })
 })
 
--- route.ts
+++ route.ts
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 export default function route(pageContext: { urlPathname: string; expressRouteProvidedByUser: string }) {
   // pageContext.expressRouteProvidedByUser would, for example, be provided by renderPage({ expressRouteProvidedByUser }) in your Express.js middleware
   const route = pageContext.expressRouteProvidedByUser
@@ -11,57 +13,110 @@ export default function route(pageContext: { urlPathname: string; expressRoutePr
 }
 
 function router(route: string, urlPathname: string): false | string[] {
-  let globs: string[] = []
+  const routeSegments = splitRouteString(route)
+  /*
+  console.log()
+  console.log('urlPathname', urlPathname)
+  console.log('route', route)
+  console.log('routeSegments', routeSegments)
+  */
+  let urlRest = urlPathname
 
-  const routeSegments = route.split('/')
-  const urlSegments = urlPathname.split('/')
-
-  let urlIndex = 0
+  const globs: string[] = []
   let isGlobbing:
     | false
     | {
         match: string[]
         isTrailingGlob?: true
       } = false
+  const pushGlob = () => {
+    if (isGlobbing) {
+      globs.push(isGlobbing.match.join('/'))
+      isGlobbing = false
+    }
+  }
+
   for (const routeIndex in routeSegments) {
     const routeSegment = routeSegments[routeIndex]
-
-    if (routeSegment === '*') {
+    /*
+    console.log('routeSegment', routeSegment)
+    console.log('urlRest', urlRest)
+    */
+    if (routeSegment.static) {
+      const { s } = routeSegment
+      if (!isGlobbing) {
+        if (!urlRest.startsWith(s)) return false
+        urlRest = urlRest.slice(s.length)
+      } else {
+        if (!urlRest.includes(s)) return false
+        const [match, ...rest] = urlRest.split(s)
+        isGlobbing.match.push(match)
+        pushGlob()
+        urlRest = rest.join(s)
+      }
+    } else if (routeSegment.param) {
+      // TODO
+    } else {
+      assert(routeSegment.glob)
+      pushGlob()
       isGlobbing = { match: [] }
       if (routeIndex === String(routeSegments.length - 1)) {
         isGlobbing.isTrailingGlob = true
-      } else {
-        continue
-      }
-    }
-
-    while (true) {
-      const urlSegment = urlSegments[urlIndex]
-      urlIndex++
-      if (urlSegment === undefined) {
-        if (!isGlobbing || !isGlobbing.isTrailingGlob) {
-          return false
-        } else {
-          globs.push(isGlobbing.match.join('/'))
-          isGlobbing = false
-          break
-        }
-      }
-      if (urlSegment !== routeSegment) {
-        if (!isGlobbing) {
-          return false
-        } else {
-          isGlobbing.match.push(urlSegment)
-        }
-      } else {
-        if (isGlobbing) {
-          globs.push(isGlobbing.match.join('/'))
-          isGlobbing = false
-        }
-        break
       }
     }
   }
 
+  if (urlRest) {
+    if (!isGlobbing) {
+      return false
+    } else {
+      isGlobbing.match.push(urlRest)
+      pushGlob()
+    }
+  }
+
   return globs
+}
+
+type Segment =
+  | {
+      glob: true
+      static?: undefined
+      param?: undefined
+    }
+  | {
+      glob?: undefined
+      static: true
+      param?: undefined
+      s: string
+    }
+  | {
+      glob?: undefined
+      static?: undefined
+      param: true
+      s: string
+    }
+function splitRouteString(s: string) {
+  const segments: Segment[] = []
+  const parts = s.split('/')
+  parts.forEach((s, i) => {
+    if (i !== parts.length - 1) s += '/'
+    if (s.startsWith('@')) {
+      segments.push({ param: true, s })
+    } else {
+      const parts = s.split('*')
+      parts.forEach((s, i) => {
+        if (i !== 0) segments.push({ glob: true })
+        if (s !== '') {
+          const segmentLast = segments[segments.length - 1]
+          if (segmentLast?.static) {
+            segmentLast.s += s
+          } else {
+            segments.push({ static: true, s })
+          }
+        }
+      })
+    }
+  })
+  return segments
 }
